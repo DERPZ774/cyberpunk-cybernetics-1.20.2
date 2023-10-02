1cf82c6a9ed7a9f1e91780fa127c5124323bdbc0
DERPZ774 <86765223+DERPZ774@users.noreply.github.com>
2023-10-01 22:26:36 -0400

    Initial commit

---
+++ src/main/java/com/derpz/cyberpunkcybernetics/CyberpunkCyberneticsClient.java
@@ -0,0 +1,10 @@
+package com.derpz.cyberpunkcybernetics;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CyberpunkCyberneticsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
